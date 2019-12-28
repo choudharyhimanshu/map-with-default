f511073d57e8446077657986aaefad5be0be32e8
Himanshu Choudhary <himanshuchoudhary@live.com>
2019-12-28 16:09:38 +0530

    Added set, has, size, clear and delete methods

--- src/__tests__/MapWIthDefault.test.ts
+++ src/__tests__/MapWIthDefault.test.ts
@@ -16,3 +16,31 @@ test('Map to return default value for invalid key', () => {
 
   expect(mapWithDefault.get('blah')).toBe(defaultValue);
 });
+
+test('Map to return true for valid key', () => {
+  const key = 'foo';
+  const value = 'bar';
+
+  const mapWithDefault = new MapWithDefault<string, string>([[key, value]], '');
+
+  expect(mapWithDefault.has(key)).toBe(true);
+});
+
+test('Map to reflect size on insert, delete and clear', () => {
+  const defaultValue: string = 'foobar';
+  const mapWithDefault = new MapWithDefault<string, string>([], defaultValue);
+
+  expect(mapWithDefault.size()).toBe(0);
+
+  mapWithDefault.set('a', 'b');
+  expect(mapWithDefault.size()).toBe(1);
+
+  mapWithDefault.set('b', 'c');
+  expect(mapWithDefault.size()).toBe(2);
+
+  mapWithDefault.delete('a');
+  expect(mapWithDefault.size()).toBe(1);
+
+  mapWithDefault.clear();
+  expect(mapWithDefault.size()).toBe(0);
+});
--- src/index.ts
+++ src/index.ts
@@ -13,6 +13,46 @@ export default class MapWithDefault<K, V> {
    */
   public get(key: K): V {
     const value = this.map.get(key);
-    return value ? value : this.defaultValue;
+    return typeof value === 'undefined' ? this.defaultValue : value;
+  }
+
+  /**
+   * @param key Key to set in the map
+   * @param value Value to set for the key
+   * @return void
+   */
+  public set(key: K, value: V): void {
+    this.map = this.map.set(key, value);
+  }
+
+  /**
+   * @param key Key to check in the map
+   * @return Boolean if the key is present or not
+   */
+  public has(key: K): boolean {
+    return this.map.has(key);
+  }
+
+  /**
+   * @returns Number of keys present in the map
+   */
+  public size(): number {
+    return this.map.size;
+  }
+
+  /**
+   * @description Clears the map
+   * @returns void
+   */
+  public clear(): void {
+    this.map.clear();
+  }
+
+  /**
+   * @param key Key to delete from the map
+   * @return Boolean if the key was present or not
+   */
+  public delete(key: K): boolean {
+    return this.map.delete(key);
   }
 }
